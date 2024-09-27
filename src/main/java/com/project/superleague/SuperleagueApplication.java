db40377b4059d372d59aea383579dd9d6ecfe664
kbatsis <gplex10@yahoo.com>
2024-09-27 12:11:58 +0300

    Initial commit.

---
+++ src/main/java/com/project/superleague/SuperleagueApplication.java
@@ -0,0 +1,13 @@
+package com.project.superleague;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperleagueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperleagueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/superleague/SuperleagueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.superleague;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperleagueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
